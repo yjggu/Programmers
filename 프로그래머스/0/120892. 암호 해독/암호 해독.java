6df55456983e756267ca1e007cfba43c670071c5
JY <162386529+yjggu@users.noreply.github.com>
2024-09-02 15:54:54 +0900

    [level 0] Title: 암호 해독, Time: 21.77 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,12 +1,12 @@
 class Solution {
     public String solution(String cipher, int code) {
-        // cipher : 암호 / code : 배수번째
-        StringBuilder str = new StringBuilder();
-
-        for (int i = code - 1; i < cipher.length(); i += code) {
-            str.append(cipher.charAt(i));
+        String result = "";
+        
+        for(int i=0; i<cipher.length(); i++){
+            if((i+1) % code == 0){
+                result += cipher.charAt(i);
+            }
         }
-
-        return str.toString();
+        return result;
     }
 }

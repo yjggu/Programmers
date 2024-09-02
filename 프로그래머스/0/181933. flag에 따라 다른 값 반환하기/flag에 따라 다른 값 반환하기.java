e665a86be6819966c87eaf3229b4d7f51786eb75
JY <162386529+yjggu@users.noreply.github.com>
2024-09-02 14:39:41 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.05 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int a, int b, boolean flag) {
-
-       if(flag){
-           return a+b;
-       }
+        if(flag) {
+            return a+b;
+        }
         return a-b;
     }
 }

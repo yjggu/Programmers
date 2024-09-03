e4b45aaba5e288a3a95ccb19d234e6582a43b26f
JY <162386529+yjggu@users.noreply.github.com>
2024-09-03 14:08:14 +0900

    [level 0] Title: 카운트 다운, Time: 0.08 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] result = new int[start_num-end_num+1];
-        int index = 0;
+        List<Integer> list = new ArrayList<>();
         
         for(int i=start_num; i>=end_num; i--) {
-            result[index] = i;  // 현재 숫자를 배열에 저장
-            index++;  // 다음 배열 인덱스로 이동
+            list.add(i);
+        }
+        
+        int length = start_num - end_num + 1;
+        int[] result = new int[length];
+        
+        for(int i=0; i<length ; i++) {
+            result[i] = list.get(i);
         }
         return result;
     }
 }
-
-

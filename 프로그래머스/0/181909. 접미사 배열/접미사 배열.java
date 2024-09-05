ae401f04597413bbc9a62e13d0507e1c4d7e663c
JY <162386529+yjggu@users.noreply.github.com>
2024-09-05 16:25:48 +0900

    [level 0] Title: 접미사 배열, Time: 0.80 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] result = new String[my_string.length()];
+        
+        for(int i=0; i<my_string.length(); i++) {
+            result[i] = my_string.substring(i);
+        }
+        Arrays.sort(result);
+        return result;
+    }
+}

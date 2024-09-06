6db21fde2b1781ca778809b9c120cc6c2d1b0e0e
JY <162386529+yjggu@users.noreply.github.com>
2024-09-06 13:19:35 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,21 +1,6 @@
-class Solution {  //  myString.toCharArray   .toString().contains(
+class Solution {
     public int solution(String myString, String pat) {
-        StringBuilder sb = new StringBuilder();
-        
-        for( char ch : myString.toCharArray()) {
-            if( ch == 'A') {
-                sb.append('B');
-            } else if (ch == 'B') {
-                sb.append('A');
-            } else {
-                sb.append(ch);
-            }
-        }
-        
-        if(sb.toString().contains(pat)) {
-            return 1;
-        } else {
-            return 0;
-        }
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
     }
 }

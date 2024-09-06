aef9ee4604ecc3d5cc452621bf12dc1aef05adcd
JY <162386529+yjggu@users.noreply.github.com>
2024-09-06 14:19:53 +0900

    [level 0] Title: l로 만들기, Time: 17.54 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,18 +1,6 @@
+
 class Solution {
     public String solution(String myString) {
-        StringBuilder result = new StringBuilder();
-        
-        for (int i = 0; i < myString.length(); i++) {
-            char currentChar = myString.charAt(i);
-            
-
-            if (currentChar < 'l') {
-                result.append('l'); 
-            } else {
-                result.append(currentChar);  
-            }
-        }
-        
-        return result.toString(); 
+        return myString.replaceAll("[^l-z]", "l");
     }
 }

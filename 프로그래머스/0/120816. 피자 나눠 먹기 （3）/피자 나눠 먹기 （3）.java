4e488fdbc06568f3e301a639e2154273c8550e9f
JY <162386529+yjggu@users.noreply.github.com>
2024-09-06 12:14:09 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,16 +1,5 @@
 class Solution {
     public int solution(int slice, int n) {
-        int result = 0;
-        
-        if( n%slice == 0 ) {
-            result = n/slice;
-        } else {
-            if(n%slice > 0){
-                result = n/slice + 1 ;
-            } else {
-                result = n/slice;
-            }
-        }
-        return result;
+        return n % slice > 0 ? n/slice+1 : n/slice;
     }
 }

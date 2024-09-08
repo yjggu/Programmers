7291ed6ce9ac335f4a69a9dd0c380dbab794ab12
JY <162386529+yjggu@users.noreply.github.com>
2024-09-08 14:33:44 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int number, int n, int m) {
         
-        return (number%n==0)&(number%m==0) ? 1 : 0;
+        return (number%n==0)&&(number%m==0) ? 1 : 0;
         }
 }

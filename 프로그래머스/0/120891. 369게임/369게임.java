6ba361d357110fae3efa02ccbe3321d53aad926e
JY <162386529+yjggu@users.noreply.github.com>
2024-09-08 20:00:38 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int order) {
+        int count = 0;
+        String number = Integer.toString(order);
+        
+        for (char num : number.toCharArray()) {
+            if(num=='3' || num=='6' || num=='9') {
+                count++;
+            }
+        }
+        return count;
+    }
+}

57189204fd65847bd633a199456c2ceb807b0ce9
JY <162386529+yjggu@users.noreply.github.com>
2024-09-08 18:30:41 +0900

    [level 0] Title: 할 일 목록, Time: 0.08 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) {  
+                result.add(todo_list[i]); 
+            }
+        }
+        
+        return result.toArray(new String[0]);
+    }
+}

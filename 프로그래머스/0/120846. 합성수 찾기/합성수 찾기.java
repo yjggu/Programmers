3add85e3055e007f6ad07f65fd8981a1e36027df
JY <162386529+yjggu@users.noreply.github.com>
2024-09-09 15:49:21 +0900

    [level 0] Title: 합성수 찾기, Time: 0.15 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -4,20 +4,18 @@ import java.util.ArrayList;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        List<Integer> numList = new ArrayList<>();
         
         for(int i=1; i<=n ; i++) {
             int count =0;
-            for(int j=1; j<=n ; j++) {
+            for(int j=1; j<=i ; j++) {
                 if(i%j==0) {
                     count++;
                 }
             }
             if (count > 2) {
-                numList.add(i);
+                answer++;
             }
         }
-        answer = numList.size();
         
         return answer;
     }
